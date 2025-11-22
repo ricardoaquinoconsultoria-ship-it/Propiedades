2fc2ddd647e9888aaa8aacefa5d4f1502592d6be
ricardoaquinoconsultoria-ship-it <ricardoaquinoconsultor.ia@gmail.com>
2025-11-22 12:36:42 -0400

    Actualizar supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 // Configuración de Supabase
 const SUPABASE_URL = 'https://vbimfwzxdafuqexsnvso.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZiaW1md3p4ZGFmdXFleHNudnNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI1MjgzNDEsImV4cCI6MjAxODEwNDM0MX0.82pW2xQwOJsU0OsUJN4y8JN0VxwV7v9v8Q5Qy9Z5J5k';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZiaW1md3p4ZGFmdXFleHNudnNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM3NTY4NDksImV4cCI6MjA3OTMzMjg0OX0.8ergS1';
 
 // Inicializar Supabase
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
